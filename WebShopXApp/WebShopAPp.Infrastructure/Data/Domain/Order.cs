bd8ebf683e9c2b77ae7448a45cff99f96b4cbdb9
Boryana872bgl <b.kandimirova2021@pmg-pernik.com>
2025-12-09 12:39:48 +0200

    Upload WebShopXApp

---
+++ WebShopXApp/WebShopAPp.Infrastructure/Data/Domain/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShopAPp.Infrastructure.Data.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        public virtual Product Products { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+        public virtual ApplicationUser User { get; set; } = null!;
+
+            public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set;}
+        public decimal TotalPrice { //avtom izchislqwa totalprice 
+            get
+            { return this.Quantity * this.Price - this.Quantity * this.Price * this.Discount / 100; } 
+        }
+
+    }
+}
---
+++ WebShopXApp/WebShopXApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopXApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
